390e0bdd24e7191e181656d538130d08ce27a239
dolgopolovtd.24 <dolgopolov.td@yandex.ru>
2022-11-28 12:52:05 +0300

    java doc

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -4,20 +4,34 @@ import io.github.humbleui.jwm.*;
 
 import java.util.function.Consumer;
 
+/**
+ * Класс окна приложения
+ */
 public class Application implements Consumer<Event> {
+    /**
+     * окно приложения
+     */
     private final Window window;
 
+    /**
+     * Конструктор окна приложения
+     */
     public Application() {
         window = App.makeWindow();
         window.setEventListener(this);
         window.setVisible(true);
     }
 
+    /**
+     * Обработчик событий
+     *
+     * @param e событие
+     */
     @Override
     public void accept(Event e) {
         if (e instanceof EventWindowClose) {
             App.terminate();
-        }else if (e instanceof EventWindowCloseRequest) {
+        } else if (e instanceof EventWindowCloseRequest) {
             window.close();
         }
     }
