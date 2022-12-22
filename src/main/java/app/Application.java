f9d2027b1bb321b9ab2fae9d510c2203bb32c482
dolgopolovtd.24 <dolgopolov.td@yandex.ru>
2022-12-22 12:44:16 +0300

    4 panels

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -9,6 +9,10 @@ import io.github.humbleui.skija.RRect;
 import io.github.humbleui.skija.Surface;
 import misc.CoordinateSystem2i;
 import misc.Misc;
+import panels.PanelControl;
+import panels.PanelHelp;
+import panels.PanelLog;
+import panels.PanelRendering;
 
 import java.io.File;
 import java.util.function.Consumer;
@@ -45,6 +49,26 @@ public class Application implements Consumer<Event> {
      * радиус скругления элементов
      */
     public static final int C_RAD_IN_PX = 4;
+
+    /**
+     * панель легенды
+     */
+    private final PanelHelp panelHelp;
+
+    /**
+     * панель курсора мыши
+     */
+    private final PanelControl panelControl;
+
+    /**
+     * панель рисования
+     */
+    private final PanelRendering panelRendering;
+
+    /**
+     * панель событий
+     */
+    private final PanelLog panelLog;
 
     /**
      * окно приложения
@@ -89,6 +113,26 @@ public class Application implements Consumer<Event> {
         label3 = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
                 4, 4, 2, 0, 1, 1, "Это тоже заголовок", true, true);
 
+        // создаём панель рисования
+        panelRendering = new PanelRendering(
+                window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, 5, 3, 0, 0,
+                3, 2
+        );
+        // создаём панель управления
+        panelControl = new PanelControl(
+                window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, 5, 3, 3, 0,
+                2, 2
+        );
+        // создаём панель лога
+        panelLog = new PanelLog(
+                window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, 5, 3, 0, 2,
+                3, 1
+        );
+        // создаём панель помощи
+        panelHelp = new PanelHelp(
+                window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, 5, 3, 3, 2,
+                2, 1
+        );
 
         if (window._layer == null)
             throw new RuntimeException("Нет доступных слоёв для создания");
@@ -119,14 +163,15 @@ public class Application implements Consumer<Event> {
      * @param windowCS СК окна
      */
     public void paint(Canvas canvas, CoordinateSystem2i windowCS) {
+        // запоминаем изменения (пока что там просто заливка цветом)
         canvas.save();
+        // очищаем канвас
         canvas.clear(APP_BACKGROUND_COLOR);
-        // рисуем первый заголовок
-        label.paint(canvas, windowCS);
-        // рисуем второй заголовок
-        label2.paint(canvas, windowCS);
-        // рисуем третий заголовок
-        label3.paint(canvas, windowCS);
+        // рисуем панели
+        panelRendering.paint(canvas, windowCS);
+        panelControl.paint(canvas, windowCS);
+        panelLog.paint(canvas, windowCS);
+        panelHelp.paint(canvas, windowCS);
         canvas.restore();
     }
 }
