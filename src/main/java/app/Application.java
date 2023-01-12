82a02b685ab385ac6fed22eef123191534b90c9f
dolgopolovtd.24 <dolgopolov.td@yandex.ru>
2023-01-12 12:55:56 +0300

    adding points works

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -81,27 +81,6 @@ public class Application implements Consumer<Event> {
      */
     public Application() {
         window = App.makeWindow();
-        window.setEventListener(this);
-        window.setTitle("Java 2D");
-        window.setWindowSize(900, 900);
-        window.setWindowPosition(100, 100);
-        switch (Platform.CURRENT) {
-            case WINDOWS -> window.setIcon(new File("src/main/resources/windows.ico"));
-            case MACOS -> window.setIcon(new File("src/main/resources/macos.icns"));
-        }
-        String[] layerNames = new String[]{
-                "LayerGLSkija", "LayerRasterSkija"
-        };
-        for (String layerName : layerNames) {
-            String className = "io.github.humbleui.jwm.skija." + layerName;
-            try {
-                Layer layer = (Layer) Class.forName(className).getDeclaredConstructor().newInstance();
-                window.setLayer(layer);
-                break;
-            } catch (Exception e) {
-                System.out.println("Ошибка создания слоя " + className);
-            }
-        }
 
         label = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
                 4, 4, 1, 1, 1, 1, "Привет, мир!", true, true);
@@ -134,6 +113,27 @@ public class Application implements Consumer<Event> {
                 2, 1
         );
 
+        window.setEventListener(this);
+        window.setTitle("Java 2D");
+        window.setWindowSize(900, 900);
+        window.setWindowPosition(100, 100);
+        switch (Platform.CURRENT) {
+            case WINDOWS -> window.setIcon(new File("src/main/resources/windows.ico"));
+            case MACOS -> window.setIcon(new File("src/main/resources/macos.icns"));
+        }
+        String[] layerNames = new String[]{
+                "LayerGLSkija", "LayerRasterSkija"
+        };
+        for (String layerName : layerNames) {
+            String className = "io.github.humbleui.jwm.skija." + layerName;
+            try {
+                Layer layer = (Layer) Class.forName(className).getDeclaredConstructor().newInstance();
+                window.setLayer(layer);
+                break;
+            } catch (Exception e) {
+                System.out.println("Ошибка создания слоя " + className);
+            }
+        }
         if (window._layer == null)
             throw new RuntimeException("Нет доступных слоёв для создания");
         window.setVisible(true);
@@ -154,6 +154,9 @@ public class Application implements Consumer<Event> {
             Surface s = ee.getSurface();
             paint(s.getCanvas(), new CoordinateSystem2i(s.getWidth() , s.getHeight() ));
         }
+        panelControl.accept(e);
+        panelRendering.accept(e);
+        panelLog.accept(e);
     }
 
     /**
--- src/main/java/panels/Panel.java
+++ src/main/java/panels/Panel.java
@@ -4,6 +4,7 @@ import io.github.humbleui.jwm.*;
 import io.github.humbleui.skija.Canvas;
 import io.github.humbleui.skija.Paint;
 import misc.CoordinateSystem2i;
+import misc.Vector2i;
 
 import java.util.function.Consumer;
 
@@ -32,6 +33,19 @@ public abstract class Panel implements Consumer<Event> {
     protected final int backgroundColor;
 
     /**
+     * последнее движение мыши
+     */
+    protected Vector2i lastMove = new Vector2i(0, 0);
+    /**
+     * было ли оно внутри панели
+     */
+    protected boolean lastInside = false;
+    /**
+     * последняя СК окна
+     */
+    protected CoordinateSystem2i lastWindowCS;
+
+    /**
      * Конструктор панели
      *
      * @param window          окно
@@ -44,6 +58,18 @@ public abstract class Panel implements Consumer<Event> {
         this.drawBG = drawBG;
         this.backgroundColor = backgroundColor;
         this.padding = padding;
+    }
+
+    /**
+     * Проверка, содержит ли панель координаты
+     *
+     * @param pos положение
+     * @return флаг, содержит или нет
+     */
+    public boolean contains(Vector2i pos) {
+        if (lastWindowCS != null)
+            return lastWindowCS.checkCoords(pos);
+        return false;
     }
 
     /**
@@ -71,6 +97,8 @@ public abstract class Panel implements Consumer<Event> {
         paintImpl(canvas, windowCS);
         // восстанавливаем область рисования
         canvas.restore();
+        // сохраняем СК окна
+        lastWindowCS = windowCS;
     }
 
     /**
@@ -80,5 +108,20 @@ public abstract class Panel implements Consumer<Event> {
      * @param windowCS СК окна
      */
     public abstract void paintImpl(Canvas canvas, CoordinateSystem2i windowCS);
+    /**
+     * Обработчик событий
+     * при перегрузке обязателен вызов реализации предка
+     *
+     * @param e событие
+     */
+    @Override
+    public void accept(Event e) {
+        if (e instanceof EventMouseMove ee) {
+            // сохраняем последнее положение мыши
+            lastMove = new Vector2i(ee);
+            // сохраняем флаг, был ли курсор внутри панели
+            lastInside = contains(lastMove);
+        }
+    }
 
 }
