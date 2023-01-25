f19f91a8b2645a989401bcd214a71abfe1b43c51
Tumka-tum <dolgopolov.td@yandex.ru>
2023-01-25 21:08:58 +0300

    multiline label

--- src/main/java/app/Colors.java
+++ src/main/java/app/Colors.java
@@ -20,6 +20,11 @@ public class Colors {
     public static final int APP_BACKGROUND_COLOR = Misc.getColor(255, 38, 70, 83);
 
     /**
+     * Цвет текста
+     */
+    public static final int MULTILINE_TEXT_COLOR = Misc.getColor(64, 255, 255, 255);
+
+    /**
      * Запрещённый конструктор
      */
     private Colors() {
