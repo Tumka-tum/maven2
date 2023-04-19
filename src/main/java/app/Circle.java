1d63fe5169cf2964a7ffeb11806f34cbd563a023
Tumka-tum <dolgopolov.td@yandex.ru>
2023-04-19 20:31:40 +0300

    home

--- src/main/java/app/Circle.java
+++ src/main/java/app/Circle.java
@@ -6,41 +6,46 @@ import java.util.Objects;
 
 public class Circle {
     Point centre;
-    Point pointR;
+    Point circ;
 
     double rad;
 
-    public Circle(Point centre, Point pointR) {
+    public Circle(Point centre, Point circ) {
         this.centre = centre;
-        this.pointR = pointR;
-        // ToDo: рассчитать радиус окружности
-        rad = 0;
+        this.circ = circ;
+        //рассчитать радиус окружности
+        rad = Math.sqrt((this.circ.pos.x-this.centre.pos.x)*(this.circ.pos.x-this.centre.pos.x) + (this.circ.pos.y-this.centre.pos.y)*(this.circ.pos.y-this.centre.pos.y));
     }
 
     Line cross(Line l){
         // если линия не пересекает, возвращаем null
         // иначе создаём новую линию на основе переданной в аргументах (Line l),
         // но образующие точки меняются на новые, которые явялются точками пересечения окружности и исходной линии
+        if (Math.abs(a*centre.pos.x + b*centre.pos.y + c)/Math.sqrt(a*a+b*b) <= rad){
+            //найти координаты точек пересечения
+        } else {
+            return null;
+        }
 
         // пусть получены две точки пересечения cA, cB
         Vector2d cA = new Vector2d(0,0);
         Vector2d cB = new Vector2d(0,0);
 
 
-        Line l = new Line(cA,cB);
+        Line line = new Line(cA,cB);
 
-        return new Line(cA,cB);
+        return line;
     }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Circle circle = (Circle) o;
-        return Objects.equals(centre, circle.centre) && Objects.equals(pointR, circle.pointR);
+        return Objects.equals(centre, circle.centre) && Objects.equals(circ, circle.circ);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(centre, pointR);
+        return Objects.hash(centre, circ);
     }
 }
--- src/main/java/app/Line.java
+++ src/main/java/app/Line.java
@@ -15,27 +15,27 @@ public class Line {
         this.pointA = pointA;
         this.pointB = pointB;
         // коэффициенты канонического уравнения прямой: ax+by+c=0
-        a =0 ;
-        b =0 ;
-        c =0 ;
+        a = this.pointB.pos.y-this.pointA.pos.y;
+        b = this.pointA.pos.x-this.pointB.pos.x;
+        c = this.pointA.pos.y*this.pointB.pos.x-this.pointB.pos.y*this.pointA.pos.x;
     }
 
-    public double getDistance() {
-//        double xA = this.pointA.pos.x;
-//        double xB = this.pointB.pos.x;
-//        double yA = this.pointA.pos.y;
-//        double yB = this.pointB.pos.y;
-//        double xC = pointC.pos.x;
-//        double yC = pointC.pos.y;
-//        double a = yB-yA;
-//        double b = xA-xB;
-//        double c = yA*xB-yB*xA;
-//        double dist = Math.abs(a*xC+b*yC+c)/Math.sqrt(a*a+b*b);
+    /*public double getDistance() {
+        double xA = this.pointA.pos.x;
+        double xB = this.pointB.pos.x;
+        double yA = this.pointA.pos.y;
+        double yB = this.pointB.pos.y;
+        double xC = pointC.pos.x;
+        double yC = pointC.pos.y;
+        double a = yB-yA;
+        double b = xA-xB;
+        double c = yA*xB-yB*xA;
+        double dist = Math.abs(a*xC+b*yC+c)/Math.sqrt(a*a+b*b);
 
         double dx = pointA.pos.x - pointB.pos.x;
         double dy = pointA.pos.y - pointB.pos.y;
         return Math.abs(dx * dx + dy * dy);
-    }
+    } */
 
     @Override
     public boolean equals(Object o) {
